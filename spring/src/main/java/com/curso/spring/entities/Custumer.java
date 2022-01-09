4d588d6a14a829bd4e702b3bfced73096638b85f
frdeoses <fdoses@outlook.es>
2022-01-09 14:54:34 +0100

    Init

---
+++ spring/src/main/java/com/curso/spring/entities/Custumer.java
@@ -0,0 +1,16 @@
+package com.curso.spring.entities;
+
+public class Custumer {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+
+}
