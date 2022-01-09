a528962d924420a1c6a68a615f77aa1f6eafb662
frdeoses <fdoses@outlook.es>
2022-01-09 15:04:49 +0100

    Create Entities

--- spring/src/main/java/com/curso/spring/entities/Custumer.java
+++ spring/src/main/java/com/curso/spring/entities/Custumer.java
@@ -1,15 +1,27 @@
 package com.curso.spring.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
 public class Custumer {
 
+    @Getter @Setter
     private String firstName;
 
+    @Getter @Setter
     private String lastName;
 
+    @Getter @Setter
     private String email;
 
+    @Getter @Setter
     private String phone;
 
+    @Getter @Setter
     private String address;
 
 
