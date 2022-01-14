373840332a5db7c7f8b718620fb8895a4a58c9ed
frdeoses <fdoses@outlook.es>
2022-01-14 19:19:44 +0100

    Create service and interface

---
+++ spring/src/main/java/com/curso/spring/services/ISupplierService.java
@@ -0,0 +1,17 @@
+package com.curso.spring.services;
+
+import com.curso.spring.entities.Custumer;
+import com.curso.spring.entities.Supplier;
+
+import java.util.List;
+
+public interface ISupplierService {
+
+    List<Supplier> getAll();
+
+    Supplier getById(Long id);
+
+    void remove(Long id);
+
+    void save(Supplier custumer);
+}
