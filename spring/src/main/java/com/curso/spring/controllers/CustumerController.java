e70f576e14c3a45c0feccb955f38a5cbafd4dd87
frdeoses <fdoses@outlook.es>
2022-01-14 19:18:19 +0100

    Update ccontroller Customer

--- spring/src/main/java/com/curso/spring/controllers/CustumerController.java
+++ spring/src/main/java/com/curso/spring/controllers/CustumerController.java
@@ -1,22 +1,39 @@
 package com.curso.spring.controllers;
 
 import com.curso.spring.entities.Custumer;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.curso.spring.services.ICustumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CustumerController {
 
+    @Autowired
+    private ICustumerService service;
+
     @GetMapping("/api/custumers")
     public List<Custumer> getAll(){
-        List<Custumer> custumers = new ArrayList<>();
 
-        custumers.add(new Custumer());
+        return service.getAll();
+    }
 
-        return custumers;
+    @GetMapping("/api/custumers/{id}")
+    public Custumer get(@PathVariable String id){
+
+        return service.getById(Long.parseLong(id));
+    }
+
+    @PostMapping("/api/custumers")
+    public void save(@RequestBody Custumer custumer){
+
+        service.save(custumer);
+    }
+
+    @DeleteMapping("/api/custumers/{id}")
+    public void remove(@PathVariable String id){
+
+        service.remove(Long.parseLong(id));
     }
 }
